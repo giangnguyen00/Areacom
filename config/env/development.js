161c978bac4f90a872b01f3d37eb870299689ac8
Giang Nguyen <gianghnguyen@ufl.edu>
2015-03-21 12:31:49 -0400

    fixing calendar

--- config/env/development.js
+++ config/env/development.js
@@ -18,7 +18,7 @@ module.exports = {
 		}
 	},
 	app: {
-		title: 'MEAN.JS - Development Environment'
+		title: 'AREA COMMUNICATON-Contract Management Application'
 	},
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
