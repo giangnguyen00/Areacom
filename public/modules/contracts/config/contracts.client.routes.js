74e80612f233e267efb133844c2aa24ae785170b
Josh <joshcamp@ufl.edu>
2015-04-17 13:47:58 -0400

    changed name of contracts page back to what it was before, as the change I made ended up being unnecessary

--- public/modules/contracts/config/contracts.client.routes.js
+++ public/modules/contracts/config/contracts.client.routes.js
@@ -6,19 +6,19 @@ angular.module('contracts').config(['$stateProvider',
 		// Contracts state routing
 		$stateProvider.
 		state('listContracts', {
-			url: '/dashboard/contracts',
+			url: '/contracts',
 			templateUrl: 'modules/contracts/views/list-contracts.client.view.html'
 		}).
 		state('createContract', {
-			url: '/dashboard/contracts/create',
+			url: '/contracts/create',
 			templateUrl: 'modules/contracts/views/create-contract.client.view.html'
 		}).
 		state('viewContract', {
-			url: '/dashboard/contracts/:contractId',
+			url: '/contracts/:contractId',
 			templateUrl: 'modules/contracts/views/view-contract.client.view.html'
 		}).
 		state('editContract', {
-			url: '/dashboard/contracts/:contractId/edit',
+			url: '/contracts/:contractId/edit',
 			templateUrl: 'modules/contracts/views/edit-contract.client.view.html'
 		});
 	}
